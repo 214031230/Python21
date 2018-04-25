7322bb7787baec51f0a0b808acc246317f44b95e
xuxu <21403123@qq.com>
2018-04-25 23:58:19 +0800

    day4

--- day4/作业/index.py
+++ day4/作业/index.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+
+def file_to_list(table, db="my_db"):
+    with open("./%s/%s" % (db, table)) as f1:
+        n = 0
+        t_info = []
+        for i in f1:
+            if n == 0:
+                t_title = i.strip().split(",")
+            else:
+                info = i.strip().split(",")
+                t_info.append(info)
+            n += 1
+    return t_title, t_info
+
+def select(num, t_title, t_info):
+    if num == "*":
+        print("{:<8} {:<8} {:<8} {:<16} {}".format(*t_title))
+        for i in t_info:
+            print("{:<8} {:<8} {:<8} {:<16} {}".format(*i))
+    if num == "name":
+        print("{:<8}".format(t_title[1]))
+        for i in t_info:
+            print("{:<8}".format(i[1]))
+
+# cmd = "select * from user_info"
+while True:
+    cmd = input(">>>")
+    cmd_res = cmd.strip().split()
+    res = file_to_list(cmd_res[3])
+    select(cmd_res[1], res[0], res[1])
+
+
+# print("{:>8} {:>8} {:>8} {:>8} {:>8} {:>8}".format(*))
+
+
