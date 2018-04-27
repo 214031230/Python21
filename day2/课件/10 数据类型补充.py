c77403660187dad2e36b3a6c1093c5823b33d9fc
xuxu <214031230@qq.com>
2018-04-27 22:27:31 +0800

    windows_home

--- day2/课件/10 数据类型补充.py
+++ day2/课件/10 数据类型补充.py
@@ -1,7 +1,6 @@
 # l1 = ['alex', 'wusir', 'taibai', 'barry', '老男孩']
-# 1
 # del l1[1::2]
-# print(l1)
+#
 #
 # for i in range(len(l1)):
 #     print(l1)  # ['alex', 'wusir', 'taibai', 'barry', '老男孩']
@@ -15,7 +14,7 @@
 #              # ['alex', 'taibai', 'barry', '老男孩']
 #             # ['alex', 'taibai', 'barry']
 #     print(i) # 0 1
-#再循环一个列表时，不要对列表进行删除的动作(改变列表元素的个数动作)，会出错
+# 再循环一个列表时，不要对列表进行删除的动作(改变列表元素的个数动作)，会出错
 
 #range 可定制的数字列表
 # for i in range(10):
@@ -53,12 +52,12 @@
 #     del dic[i]
 # print(dic)
 
-#tu 如果元组里面只有一个元素并且没有逗号隔开，那么他的数据类型与该元素一致。
-# tu1 = (1)
-# print(tu1,type(tu1)) # 1 <class 'int'>
-#
-# tu2 = ('alex')
-# print(tu2,type(tu2)) # alex <class 'str'>
-#
-# tu3 = (['alex',1,2])
-# print(tu3,type(tu3)) # ['alex', 1, 2] <class 'list'>
+# tu 如果元组里面只有一个元素并且没有逗号隔开，那么他的数据类型与该元素一致。
+tu1 = (1)
+print(tu1,type(tu1)) # 1 <class 'int'>
+
+tu2 = ('alex')
+print(tu2,type(tu2)) # alex <class 'str'>
+
+tu3 = (['alex',1,2])
+print(tu3,type(tu3)) # ['alex', 1, 2] <class 'list'>
--- day4/课件/7.递归.py
+++ day4/课件/7.递归.py
@@ -1,55 +1,70 @@
-# def func():
-#     print(1)
-#     func()
-#
-# func()    # 997 /998
+# 递归的定义——在一个函数里再调用这个函数本身，实际操作中递归必须要有个可停止的条件。
+# 递归解析为递推和回溯两步，先递推当满足条件以后开始回溯
+# 递归的最大深度—997
+# RecursionError: maximum recursion depth exceeded while calling a Python object
+def func():
+    print(1)
+    func()
+func()
 
-# import sys
-# def foo(n):
-#     print(n)
-#     n += 1
-#     foo(n)
-# foo(1)
+#  测试递归的最大深度
+def foo(n):
+    print(n)
+    n += 1
+    foo(n)
+foo(1)
 
-# 6!
-# print(6*5*4*3*2*1)
-# def fn(n):
-#     if n == 1:return 1
-#     return n*fn(n-1)
-# print(fn(6))
+import sys
+#  修改递归的最大深度，一般不修改，如果默认递归深度解决不了 说明这个问题不能用递归解决
+sys.setrecursionlimit(2000)
 
-# 递归 就是自己调用自己
-# 递归需要有一个停止的条件
-# def fn(6):
-#     if 6 == 1:return 1
-#     return 6*fn(5)
-# print(fn(6))
-#
-# def fn(5):
-#     return 5*fn(4)
-#
-# def fn(4):
-#     return 4*fn(3)
-#
-# def fn(3):
-#     return 3*fn(2)
-#
-# def fn(2):
-#     return 2*fn(1)
-#
-# def fn(1):
-#     return 1
+# 用递归计算6*5*4*3*2*1的乘积
+print(6*5*4*3*2*1)
+def fn(n):
+    if n == 1:return 1
+    return n*fn(n-1)
+print(fn(6))
 
-# li = [1, [2, [3, [4, [5]]]]]
-#
-# def func(li):
-#     for i in li:
-#         if type(i) is list:
-#             func(i)
-#         else:
-#             print(i)
-# func(li)
+# 第一次fn(6)  # 返回值 6*fn(6-1)= 720
+def fn(n): # n = 6
+    if 6 == 1:return 1
+    return 6*fn(6-1)
 
-# li = []
-# if type(li) is list:
-#     print("ok")
+# 第二次fn(6-1) # 返回值 5*fn(5-1) = 120
+def fn(n): # n = 5
+    if 5 == 1:return 1
+    return 5*fn(5-1)
+
+# 第三次fn(5-1)  # 返回值 4*fn(4-1) = 24
+def fn(n): # n = 4
+    if 4 == 1:return 1
+    return 4*fn(4-1)
+
+# 第四次fn(4-1) # 返回值 3*fn(3-1) = 6
+def fn(n): # n = 3
+    if 3 == 1:return 1
+    return 3*fn(3-1)
+
+# 第五次fn(3-1) # 返回值 2*fn(2-1) = 2
+def fn(n): # n = 2
+    if 2 == 1:return 1
+    return 2*fn(2-1)
+
+# 第六次fn(2-1)   返回值 fn(2-1）= 1
+def fn(n): # n = 2
+    if 1 == 1:return 1
+    return 2*fn(2-1)
+
+
+# 练习 打印列表的元素
+li = [1, [2, [3, [4, [5]]]]]
+
+def fun(li):
+    for i in li:
+        if type(i) == list:
+            fun(i)
+        else:
+            print(i)
+fun(li)
+
+
