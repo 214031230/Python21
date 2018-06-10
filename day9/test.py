20a6c87c20e02279825d3f33e26fa479513631ca
xuxu <21403123@qq.com>
2018-06-10 23:23:19 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Homework/day06/.idea/workspace.xml
    #	Homework/day6_v2/bin/start.py
    #	Homework/day6_v2/core/main.py
    #	Homework/day6_v2/core/manager.py
    #	Homework/day6_v2/core/my_pickle.py
    #	Homework/day6_v2/core/school.py
    #	Homework/day6_v2/core/student_manager.py
    #	Homework/day6_v2/core/teacher_manager.py
    #	Homework/day6_v2/db/classinfo
    #	Homework/day6_v2/db/courseinfo
    #	Homework/day6_v2/db/schoolinfo
    #	Homework/day6_v2/db/teacherinfo
    #	Homework/day6_v2/db/初始化数据库.py

--- day9/test.py
+++ day9/test.py
@@ -31,35 +31,129 @@ from multiprocessing import Process,Lock
 #     print("所有子进程已经执行完毕")
 
 # 抢票
+#
+# def search():
+#     with open("test_db") as f:
+#         time.sleep(random.random())
+#         res = json.load(f)
+#         print("%s剩余票数：%s" % (os.getpid(), res["count"]))
+#
+#
+# def get():
+#     with open("test_db") as f:
+#         time.sleep(random.random())
+#         res = json.loads(f.read())
+#         if res["count"] > 0:
+#             res["count"] -= 1
+#             time.sleep(random.random())
+#             print("%s:购买成功" % os.getpid())
+#             with open("test_db", "w") as f:
+#                 json.dump(res, f)
+#
+#
+# def run(lock):
+#     search()
+#     lock.acquire()
+#     get()
+#     lock.release()
+#
+#
+# if __name__ == '__main__':
+#     lock = Lock()
+#     for i in range(10):
+#         p = Process(target=run, args=(lock,))
+#         p.start()
 
-def search():
-    with open("test_db") as f:
-        time.sleep(random.random())
-        res = json.load(f)
-        print("%s剩余票数：%s" % (os.getpid(), res["count"]))
 
 
-def get():
-    with open("test_db") as f:
-        time.sleep(random.random())
-        res = json.loads(f.read())
-        if res["count"] > 0:
-            res["count"] -= 1
-            time.sleep(random.random())
-            print("%s:购买成功" % os.getpid())
-            with open("test_db", "w") as f:
-                json.dump(res, f)
+
+# 开启多进程
+# from multiprocessing import Process
+import os
+import time
 
 
-def run(lock):
-    search()
-    lock.acquire()
-    get()
-    lock.release()
+# def func():
+#     time.sleep(random.randint(1,3))
+#     print(os.getpid())
+#
+#
+# if __name__ == '__main__':
+#     lst = []
+#     for i in range(10):
+#         p = Process(target=func)
+#         p.start()
+#         lst.append(p)
+#     for p in lst:p.join()
+#     print("主进程end")
+
+# 开启多线程
+# from threading import Thread
+# import os
+# import time
+#
+#
+# def func():
+#     time.sleep(random.randint(1,3))
+#     print(os.getpid())
+#
+#
+# if __name__ == '__main__':
+#     lst = []
+#     for i in range(10):
+#         t = Thread(target=func)
+#         t.start()
+#         lst.append(t)
+#     for t in lst:t.join()
+#     print("主线程end")
+
+# 开启协程
+import gevent
+
+
+def eat():
+    print("eat1...")
+    gevent.sleep(2)
+    print("eat2...")
+
+
+def play():
+    print("plat1...")
+    gevent.sleep(1)
+    print("plat2...")
 
 
 if __name__ == '__main__':
-    lock = Lock()
-    for i in range(10):
-        p = Process(target=run, args=(lock,))
-        p.start()
+    g = gevent.spawn(eat)
+    g1 = gevent.spawn(play)
+    g.join()
+    g1.join()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
