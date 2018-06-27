0cda84d90fe069e5701fc2481f71a29ec337b5aa
lanpa <214031230@qq.com>
2018-06-27 18:12:18 +0800

    Windows

--- day10/作业/百度注册页面/register.py
+++ day10/作业/百度注册页面/register.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python3
 import socket
-server = socket.socket()
-server.bind(("127.0.0.1", 9001))
-server.listen()
-print("启动server...")
-while True:
-    conn,addr = server.accept()
-    print(conn, addr)
-    while True:
-        msg = conn.recv(1024).decode("utf-8")
-        print(msg)
-        conn.send(b"ok")
+import re
+class MyServer:
+    def __init__(self,ip,port):
+        self.server = socket.socket()
+        self.server.bind((ip, port))
+        self.server.listen()
+    def run(self):
+        """
+        启动服务
+        :return:
+        """
+        print("server is running")
+        while 1:
+            self.conn, self.addr = self.server.accept()
+            while True:
+                try:
+                    msg = self.conn.recv(1024).decode("utf-8")
+                    ret = re.search("GET.*username.*password.*HTTP", msg).group()
+                    username = re.search(r"username=.*&",ret).group().split("=")[1].split("&")[0]
+                    password = ret.split("&")[1].split()[0].split("=")[1]
+                    if self.login(username,password):
+                        self.conn.send(b"HTTP/1.1 200 OK\r\n\r\n<html><body>OK</body></html>")
+                        self.conn.close()
+                    else:
+                        self.conn.send(b"HTTP/1.1 200 OK\r\n\r\n<html><body>ERROR</body></html>")
+                        self.conn.close()
+                except Exception as e:
+                    break
+
+    def login(self,username,password):
+        print("地址:%s 用户名：%s 密码：%s" % (self.addr,username,password))
+        if username == "spf" and password == "123":
+            return  True
+        else:
+            return  False
+
+
+server = MyServer("127.0.0.1",9000)
+server.run()
+
+
