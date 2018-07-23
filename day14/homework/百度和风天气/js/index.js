4a19659483bf690155c1fcb4c5eeec5b0bc9ac5d
xuxu <214031230@qq.com>
2018-07-23 23:31:53 +0800

    Merge branch 'master' of https://github.com/214031230/Python21
    # Conflicts:
    #	Homework/day06/.idea/workspace.xml

--- day14/homework/百度和风天气/js/index.js
+++ day14/homework/百度和风天气/js/index.js
@@ -18,13 +18,31 @@ $(function () {
                 // 把字符串转换成可操作的数组
                 var jsonData = JSON.parse(data);
                 console.log(jsonData);
-                $("p").text(data)
+                $(".city").text(jsonData.HeWeather6[0].basic.parent_city+"：");
+                $(".hefeng>.cond_code").attr("src","./images/cond-icon-heweather/"+jsonData.HeWeather6[0].now.cond_code+".png")
+                $(".hefeng>.wendu").text(jsonData.HeWeather6[0].now.tmp+"℃");
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+        $.ajax({
+            url: 'https://free-api.heweather.com/s6/air/now?location=beijing&key=b73a12edfab24ef6827447dc7557f50d',
+            type: "get",
+            dataType: 'text',
+            success: function (data) {
+                // 把字符串转换成可操作的数组
+                var jsonData = JSON.parse(data);
+                console.log(jsonData);
+                $(".kongqi").text(jsonData.HeWeather6[0].air_now_city.qlty);
+                $(".zhishu").text(jsonData.HeWeather6[0].air_now_city.pm25);
             },
             error: function (err) {
                 console.log(err);
             }
         })
     };
-    var t = setInterval(run,3000);
+    run()
+    // var t = setInterval(run,3000);
 
 });
