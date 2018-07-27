f24ff60cae7aa8b51d602425c2c2ca30802350b4
lanpa <214031230@qq.com>
2018-07-27 17:39:55 +0800

    Windows

--- 复习1/day8.py
+++ 复习1/day8.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+# lst = [1,2,3,4,5,6,7]
+#
+# def fun(x):
+#     return x * x
+# print(list(filter(lambda x: x % 2 == 0,lst)))
+# print(list(map(lambda x: x % 2 == 0,lst)))
+# print(list(filter(lambda x: x * x,lst)))
+# print(list(map(lambda x: x * x,lst)))
