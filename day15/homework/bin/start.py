8193f1ccb1a4d9a94233fae8ceb7bfc9efd46c36
xuxu <21403123@qq.com>
2018-08-01 21:31:34 +0800

    MAC OS

--- day15/homework/bin/start.py
+++ day15/homework/bin/start.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from core import main
+from sys import path as sys_path
+from os import path
+sys_path.insert(0, path.dirname(path.dirname(path.abspath(__file__))))
+from core import choice
 
 if __name__ == '__main__':
-    main.choice()
+    choice.choice()
