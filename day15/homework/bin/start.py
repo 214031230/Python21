6077523b7b36efbe36b8a98629343d2f89b79088
lanpa <214031230@qq.com>
2018-08-02 14:26:11 +0800

    Windows

--- day15/homework/bin/start.py
+++ day15/homework/bin/start.py
@@ -2,7 +2,7 @@
 from sys import path as sys_path
 from os import path
 sys_path.insert(0, path.dirname(path.dirname(path.abspath(__file__))))
-from core import choice
+from core.choice import Choice as c
 
 if __name__ == '__main__':
-    choice.choice()
+    c.choice()
--- day15/homework/core/choice.py
+++ day15/homework/core/choice.py
@@ -4,71 +4,50 @@ from core import public
 import pymysql
 
 
-def choice():
-    """
-    功能选择，登录或者注册
-    :return:
-    """
-    print("""
-        请选择功能：1、登录 2、注册
-    """)
-    while 1:
-        ch = input("请选择功能(输入功能ID)：").strip()
-        if not ch:continue
-        if ch == "1":
-            start_login()
-            break
-        elif ch == "2":
-            try:
-                start_register()
-            except pymysql.err.IntegrityError:
-                print("Error:用户已存在")
-            break
-        else:
-            print("Error：请输入正确的ID")
+class Choice:
+    @staticmethod
+    def choice():
+        """
+        功能选择，登录或者注册
+        :return:
+        """
+        print("""
+            请选择功能：1、登录 2、注册
+            测试账号：spf 密码：123
+        """)
+        view = {"1": "login", "2": "register"}
+        while 1:
+            ch = input("请选择功能(输入功能ID)：").strip()
+            if not ch: continue
+            if ch in view:
+                try:
+                    Choice.start(view[ch])
+                except pymysql.err.IntegrityError:
+                    print("Error:用户名已经存在")
+                except Exception as e:
+                    print("Error: %s" % e)
+            else:
+                print("Error：请输入正确的ID")
 
-
-def start_login():
-    """
-    用户登录
-    :return:
-    """
-    log = public.Public.log()
-    count = 0
-    while count < 3:
-        user = input("user:").strip()
-        password = input("password:").strip()
-        if not user or not password:
-            print("Error:用户名密码不能为空！")
-            continue
-        loginobj = login.Login(user, password)
-        if loginobj.login():
-            print("INFO:登录成功！")
-            log.info("登录成功")
-            break
-        else:
-            print("Error:用户名或密码错误")
-            log.error("用户名或密码错误")
-
-
-def start_register():
-    """
-    用户注册
-    :return:
-    """
-    log = public.Public.log()
-    count = 0
-    while count < 3:
-        user = input("user:").strip()
-        password = input("password:").strip()
-        if not user or not password:
-            print("Error:用户名密码不能为空！")
-            continue
-        loginobj = login.Login(user, password)
-        if loginobj.register():
-            print("INFO:注册成功！")
-            log.info("注册成功")
-            break
-        else:
-            print("Error:注册失败！")
-            log.error("注册失败")
+    @staticmethod
+    def start(obj):
+        """
+        用户登录/注册
+        :return:
+        """
+        log = public.Public.log()
+        count = 0
+        while count < 3:
+            user = input("user:").strip()
+            password = input("password:").strip()
+            if not user or not password:
+                print("Error:用户名密码不能为空！")
+                continue
+            login_obj = login.Login(user, password)
+            if getattr(login_obj, obj)():
+                print("INFO: %s Success！" % obj)
+                log.info("%s Success" % obj)
+                break
+            else:
+                print("Error: %s fail" % obj)
+                log.error("Error: %s fail" % obj)
