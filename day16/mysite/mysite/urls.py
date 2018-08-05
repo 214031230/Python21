8da75e490c3e29afaf795316fd3fb4ef4060902e
xuxu <21403123@qq.com>
2018-08-05 19:45:07 +0800

    MAC OS

--- day16/mysite/mysite/urls.py
+++ day16/mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""HostManagement URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from app01 import views
-
+from app01 import views as app01_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r"^login/", views.login),
-    url(r"^index/", views.index)
+    url(r'^index/', app01_views.index)
 ]
--- project/HostManagement/app01/views.py
+++ project/HostManagement/app01/views.py
@@ -2,4 +2,26 @@ from django.shortcuts import render, HttpResponse, redirect
 
 
 def login(request):
-    pass
+    """
+    用户登录
+    :param request:
+    :return: 登录成功：返回index页面 登录失败：登录页面
+    """
+    if request.method == "POST":
+        username = request.POST.get("user")
+        password = request.POST.get("pwd")
+        if username == "spf" and password == "123":
+            return redirect("/index/")
+        else:
+            return render(request, "login.html", {"login_status": "用户名密码错误"})
+    else:
+        return render(request, "login.html", {"login_status": ""})
+
+
+def index(request):
+    """
+    index页面
+    :param request:
+    :return: 返回index页面
+    """
+    return render(request, "index.html")
