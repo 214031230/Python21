91e070d2d4100da69c419f9dc35ed3e38b560d29
lanpa <214031230@qq.com>
2018-08-10 18:18:13 +0800

    Windows

--- day16/HostManagement/static/js/platform.js
+++ day16/HostManagement/static/js/platform.js
@@ -1,10 +1,10 @@
 $(function () {
     $(".view").click(function () {
         $(this).addClass("active").siblings().removeClass("active")
-    })
+    });
     $(".adduser").click(function () {
         location.href="/add_user/"
-    })
+    });
     $(".change").click(function () {
         location.href="/user_list/"
     })
