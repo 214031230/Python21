c678c577cbfa580e8043f86409f20109c20a1b8a
xuxu <21403123@qq.com>
2018-09-03 23:17:22 +0800

    MAC OS

--- day20/homework/CMS/fault_reporting/admin.py
+++ day20/homework/CMS/fault_reporting/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from fault_reporting import models
-# Register your models here.
 
-# admin.site.register(models.UserInfo)
+admin.site.register(models.UserInfo)
+admin.site.register(models.Classify)
+admin.site.register(models.Comment)
+admin.site.register(models.Fault)
+admin.site.register(models.Tag)
--- day20/homework/CMS/fault_reporting/forms.py
+++ day20/homework/CMS/fault_reporting/forms.py
@@ -1,39 +1,41 @@
 #!/usr/bin/env python3
 from django import forms
-from django.core.validators import EmailValidator
-from django.core.exceptions import ValidationError
-from django.contrib.auth import models
+from fault_reporting import models
 
 
 class UserModelForm(forms.ModelForm):
     class Meta:
-        model = models.User
-        fields = ["username", "last_name", "email"]
+        model = models.UserInfo
+        fields = ["username", "last_name", "phone", "email"]
         labels = {
             "username": "用户名",
             "last_name": "昵称",
+            "phone": "手机号",
             "email": "邮箱"
         }
         widgets = {
             "username": forms.widgets.TextInput(attrs={"class": "form-control"}),
             "last_name": forms.widgets.TextInput(attrs={"class": "form-control"}),
+            "phone": forms.widgets.TextInput(attrs={"class": "form-control"}),
             "email": forms.widgets.EmailInput(attrs={"class": "form-control"}),
         }
 
 
 class RegisterModelForm(forms.ModelForm):
     class Meta:
-        model = models.User
-        fields = ["username", "password", "last_name", "email"]
+        model = models.UserInfo
+        fields = ["username", "password", "last_name", "phone", "email"]
         labels = {
             "username": "用户名",
             "password": "密码",
             "last_name": "昵称",
+            "phone": "手机号",
             "email": "邮箱地址"
         }
         widgets = {
             "username": forms.widgets.TextInput(attrs={"class": "form-control", "placeholder": "用户名"}),
             "password": forms.widgets.PasswordInput(attrs={"class": "form-control", "placeholder": "密码"}),
             "last_name": forms.widgets.TextInput(attrs={"class": "form-control", "placeholder": "昵称"}),
+            "phone": forms.widgets.TextInput(attrs={"class": "form-control", "placeholder": "手机号"}),
             "email": forms.widgets.EmailInput(attrs={"class": "form-control", "placeholder": "邮箱地址"}),
         }
