6121631deec324a4fbe7325c6551d275aebba396
xuxu <21403123@qq.com>
2018-10-17 21:47:00 +0800

    MAC OS

---
+++ 复习2/day1.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# print(type(1))
+# name = input("请输入名字：")
+# print(name)
+# print("""
+# name = %s
+# age = %d
+# """ % ("spf", 12))
+
+# while 1:
+#     age = int(input("猜年龄："))
+#     if age > 10:
+#         print("太大了")
+#     elif age < 10:
+#         print("太小了")
+#     else:
+#         print("恭喜你才对了")
+#         break
+# num = 1
+# while num <= 100:
+#     print(num)
+#     num += 1
+# s = 0
+# num = 1
+# for i in range(1, 101):
+#     print(i)
+#     s += i
+#
+# while num <= 100:
+#     s += num
+#     num += 1
+# print(s)
+
+# count = 0
+# while count < 100:
+#     if count == 110:
+#         break
+#     count += 1
+# else:
+#     print("循环正常结束")
+
+# s = 0
+# count = 0
+# while count <= 100:
+#     if count % 2 == 1:
+#         s += count
+#     count += 1
+# print(s)
+# print(3>4 or 4<3 and 1==1)
+# # T
+# print(1 < 2 and 3 < 4 or 1>2)
+# # T
+# print(2 > 1 and 3 < 4 or 4 > 5 and 2 < 1)
+# # T
+# print(1 > 2 and 3 < 4 or 4 > 5 and 2 > 1 or 9 < 8)
+# # print(F or F or 9 < 8)
+# # F
+# print(1 > 1 and 3 < 4 or 4 > 5 and 2 > 1 and 9 > 8 or 7 < 6)
+# # print(F or F or 7 < 6)
+# # F
+# print(not 2 > 1 and 3 < 4 or 4 > 5 and 2 > 1 and 9 > 8 or 7 < 6)
+# # print(F  or F or F)
+# # F
+
+# 1 - 2 + 3 - 4 + 5
+# s = 0
+# for i in range(1, 100):
+#     if i % 2 == 1:
+#         s += i
+#     else:
+#         s -= i
+# print(s)
